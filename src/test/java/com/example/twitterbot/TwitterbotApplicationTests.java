808c93ed646e97d9d6e8cdc8182c3c52a0f18135
seungchanbaeg <mokdonjr@gmail.com>
2022-05-26 14:51:43 +0900

    initialize spring boot

---
+++ src/test/java/com/example/twitterbot/TwitterbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twitterbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
